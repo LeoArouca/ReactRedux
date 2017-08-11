e3398ba84b7b489d76da52fac170e0724d46645e
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-11 21:12:01 +0800

    Loads list with youtube image and title

---
+++ src/components/video_list.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+// Avoid js loops, try to use iterators like map
+
+// Put props as parameter
+const VideoList = (props) => {
+
+  // Map into array, React is smart to render array as many elements
+  const videoItems = props.videos.map((video) => {
+    // common strategy is to use id, it does not need to be too creative, as long as each has one different
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
+  return(
+    <ul className='col-md-4 list-group'>
+      { videoItems }
+    </ul>
+  );
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 // ./ means current dir
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyB7ug5wuB17ns8l66Z8WcXsz7Prj6y5LBs';
 
 // Create a new component. This component should produce some HTML
 // const App = function(){
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+// Function
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>
+//   );
+// }
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = { videos: [] };
+
+    // Fetch youtube stuff
+    // YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // Standard way
+      // this.setState({ videos: videos });
+      // When have key and property/variable with the same name ES6 allows this:
+      this.setState({ videos });
+    });
+  }
+
+  render(){
+
+    // Passing props
+    // <VideoList videos={ this.state.videos } />
+
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos } />
+      </div>
+    );
+  }
 }
 
 // Take this component's HTML and put it on the page (DOM)
